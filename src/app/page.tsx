aff61980e14b31194b0ad08d2c3882934568f1f7
MAOZ <maooz4426@gmail.com>
2025-01-04 21:46:14 +0900

    aboutページ作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { LinkIcon } from "@/features/icon/components";
-import Image from "next/image";
+import {LinkIcon, ProfileIcon} from "@/features/icon/components";
 import style from "./style.module.scss";
 export default function Home() {
   return (
     <div className={style.container}>
-      <div className={style.icon}>
-        <Image src={"/icon.png"} alt="icon" width={500} height={500} />
-      </div>
-
+        <div>
+            <ProfileIcon width={500} height={500} />
+        </div>
       <div className={style.iconContainer}>
         <LinkIcon width={90} height={90} href="https://github.com/maooz4426" src="Github.svg" />
         <LinkIcon width={80} height={80} href="https://x.com/maotan426" src="X.svg" />
--- src/features/header/components/index.tsx
+++ src/features/header/components/index.tsx
@@ -1,12 +1,15 @@
 import style from "@/features/header/styles/style.module.scss";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <div className={style.headerContainer}>
       <div className={style.headerTitle}>MAOZ dev</div>
       <div className={style.menuContainer}>
-        <div className={style.headerContent}>Home</div>
-        <div className={style.headerContent}>About</div>
+        <Link className={style.headerContent} href="/">Home</Link>
+        <Link className={style.headerContent} href="/about">
+            About
+        </Link>
         <div className={style.headerContent}>Works</div>
       </div>
     </div>
