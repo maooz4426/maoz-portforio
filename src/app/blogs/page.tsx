6bbf3ff65d65c4eded3e3881045bc3e0c53e74cc
MAOZ <maooz4426@gmail.com>
2025-01-05 21:06:22 +0900

    コンポーネント作成

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { Card } from "@/components/ui/card";
+import { BlogCard } from "@/features/blog/components";
 import { fetchOgp } from "@/features/blog/funcs/ogp";
 import { fetchMyQiitaURLs } from "@/features/blog/funcs/qiita";
 import { OGP } from "@/features/blog/types";
-import Image from "next/image";
-import Link from "next/link";
+import { AnimatePresence } from "framer-motion";
 import React from "react";
 import style from "./style.module.scss";
 
@@ -34,34 +33,44 @@ const BlogsPage = () => {
 
   return (
     <>
-      <div className={style.titleContainer}>
-        <p className={style.title}>Blogs</p>
-      </div>
-      <div className={style.blogsContainer}>
-        <div className={style.blogs}>
-          {ogps?.map((ogp, index) => {
-            if (!ogp?.images?.[0]) return null;
-
-            return (
-              <Card key={index} className={style.card}>
-                <Link href={ogp.url}>
-                  {/*<CardContent>*/}
-                  <Image
-                    key={index} // もしくは一意のID
-                    width={500}
-                    height={500}
-                    src={ogp.images[0].toString()}
-                    alt={"image"}
-                    className={style.blog}
-                  />
-                  <p className={style.blogTitle}>{ogp.title}</p>
-                  {/*</CardContent>*/}
-                </Link>
-              </Card>
-            );
-          })}
+      <AnimatePresence mode="wait">
+        <div className={style.titleContainer}>
+          <p className={style.title}>Blogs</p>
         </div>
-      </div>
+        <div className={style.blogsContainer}>
+          <div className={style.blogs}>
+            {ogps?.map((ogp, index) => {
+              if (!ogp?.images?.[0]) return null;
+
+              return (
+                <BlogCard key={index} ogp={ogp} />
+                // <motion.div
+                //     initial={{opacity: 0}}
+                //     animate={{opacity: 1}}
+                //     transition={{duration: 0.5, delay: 0.5}}
+                //     key="image-container"
+                // >
+                //   <Card key={index} className={style.card}>
+                //     <Link href={ogp.url}>
+                //       {/*<CardContent>*/}
+                //       <Image
+                //           key={index} // もしくは一意のID
+                //           width={500}
+                //           height={500}
+                //           src={ogp.images[0].toString()}
+                //           alt={"image"}
+                //           className={style.blog}
+                //       />
+                //       <p className={style.blogTitle}>{ogp.title}</p>
+                //       {/*</CardContent>*/}
+                //     </Link>
+                //   </Card>
+                // </motion.div>
+              );
+            })}
+          </div>
+        </div>
+      </AnimatePresence>
     </>
   );
 };
